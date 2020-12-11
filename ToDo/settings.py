c0ed1127d49e566128b86bc716f9dde5a82900a3
arijit-s04 <74173304+arijit-s04@users.noreply.github.com>
2020-12-11 14:22:38 +0530

    Update settings.py

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -25,7 +25,7 @@ with open('E:\Django\ToDo\secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.0.102','127.0.0.1','0.0.0.0']
 
